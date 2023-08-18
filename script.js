2bfe2704f4e4d268da4886e76077bb6ef9a6a41a
beanboi57438 <140765492+beanboi57438@users.noreply.github.com>
2023-08-18 12:16:54 +1200

    charder

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var block = document.getElementById("block");
 var hole = document.getElementById("hole");
+var character = document.getElementById("character");
 
 hole.addEventListener('animationiteration', () => {
     var random = -((Math.random()*300)+150);
     hole.style.top = random + "px";
 });
+setInterval(function(){
+    var characterTop = 
+    parseIntwindow.getComputedStyle(character).getPropertyValue("top");
+},10)
