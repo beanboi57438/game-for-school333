c40757d6ad962728726545d1c51841692741e621
beanboi57438 <140765492+beanboi57438@users.noreply.github.com>
2023-08-24 10:24:29 +1200

    add jumping

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var block = document.getElementById("block");
 var hole = document.getElementById("hole");
 var character = document.getElementById("character");
+var jumping = 0;
 
 hole.addEventListener('animationiteration', () => {
     var random = -((Math.random()*300)+150);
@@ -9,5 +10,26 @@ hole.addEventListener('animationiteration', () => {
 setInterval(function(){
     var characterTop = 
     parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if(jumping==0){
     character.style.top = (characterTop+3)+"px";
+    }
 },10)
+
+function jump(){
+    jumping = 1;
+    let jumpcount = 0;
+    var jumpInterval = setInterval(function(){
+        var characterTop = 
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if((character>6)&&(counter<15)){
+        character.style.top = (characterTop-5)+"px";
+    }
+
+    if(jumpcount>20){
+        clearInterval(jumpInterval);
+        jumping=0;
+        jumpcount=0;
+    }
+        jumpcount++;
+    },10);
+}
