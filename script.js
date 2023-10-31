ab80b64a9cec5a647ee219251b25eb01443b23d5
beanboi57438 <140765492+beanboi57438@users.noreply.github.com>
2023-11-01 09:07:52 +1300

    addet comment to the java scrip

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var block = document.getElementById("block");
 var hole = document.getElementById("hole");
-var character = document.getElementById("character");
+var character = document.getElementById("character");          //all the veable for the game to run 
 var jumping = 0;
 var counter = 0;
 
@@ -15,8 +15,8 @@ setInterval(function(){
         character.style.top = (characterTop+3)+"px";
     }
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-    var cTop = -(500-characterTop);
+    var holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));           //all the code for the movement of the boxes and the holes inbetween the box for the player
+    var cTop = -(500-characterTop);                                                          // to avoid 
     if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
         alert("Game over. Score: "+(counter-1));
         character.style.top = 100 + "px";
@@ -28,12 +28,12 @@ function jump(){
     jumping = 1;
     let jumpCount = 0;
     var jumpInterval = setInterval(function(){
-        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));  //all of the jumping funcie for when the player click and the chader jumps 
         if((characterTop>8)&&(jumpCount<15)){
             character.style.top = (characterTop-5)+"px";
         }
         if(jumpCount>20){
-            clearInterval(jumpInterval);
+            clearInterval(jumpInterval);   // the time when the player click and when the player starts to drop again
             jumping=0;
             jumpCount=0;
         }
